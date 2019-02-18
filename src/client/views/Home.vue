c7a6de9bac270afb65caf2f67e65473127d3ff76
sun0718 <sun0718@qq.com>
2019-02-18 15:41:39 +0800

    项目目录优化

--- src/client/views/Home.vue
+++ src/client/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="home">
     <h1>{{ msg }}</h1>
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img alt="Vue logo" src="@/client/assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "@/client/components/HelloWorld.vue";
 
 export default {
   name: "home",
