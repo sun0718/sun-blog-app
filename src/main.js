787cfaa13e2416808a01df8318c1e7e627fd8d5c
sun0718 <sun0718@qq.com>
2019-02-19 16:33:11 +0800

    new project

--- src/main.js
+++ src/main.js
@@ -2,10 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import axios from "axios"
-
-Vue.prototype.$axios = axios;
-axios.defaults.baseURL = "http://localhost:9000";
 
 Vue.config.productionTip = false;
 
---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>An ordinary Chinese young man</h1>
+    <ul>
+      <li>He is a civil engineer</li>
+      <li>He is an architect</li>
+      <li>He is an information technology practitioner</li>
+      <li>He is an ideal and ambitious young man</li>
+      <li>He works at Ping An Group</li>
+      <li>He always believes that it is the best life to do a good job of not busy work, to live a wonderful life without saltiness</li>
+      <li>He is a person who wants to create a better life.</li>
+    </ul>
+  </div>
+</template>
