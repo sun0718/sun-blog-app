a429c04794eb109fae8444bb846139892c60786e
sunjilong1413 <sun0718@qq.com>
2019-11-17 22:38:39 +0800

    host配置

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,6 @@
 
 module.exports = {
     host:{ 
-        // baseURL: process.env.NODE_ENV == 'development' ? 'http://localhost:8080/apis' : '/api'
-        baseURL: 'http://localhost:8080/apis'
+        baseURL: process.env.NODE_ENV == 'development' ? 'http://localhost:8080/apis' : '/api'
     }
 }
